e35241077a51d06dca8e2e410e73236f3fe60458
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-07-07 03:06:43 +0200

    Added function to get the user id

--- api/verify.js
+++ api/verify.js
@@ -1,11 +1,14 @@
 const { Magic } = require("@magic-sdk/admin");
-const mAdmin = new Magic();
+const mAdmin = new Magic(process.env.ML_SECRET);
 
-module.exports = (req, res) => {
+module.exports = async(req, res) => {
     const DIDToken = req.headers.authorization.substring(7);
 
     try {
         mAdmin.token.validate(DIDToken);
+        const metadata = await mAdmin.users.getMetadataByToken(DIDToken);
+        const id = metadata.issuer;
+
         res.json({ body: "yes" });
     } catch (error) {
         res.json({ body: "no" });
--- app.js
+++ app.js
@@ -1,73 +1,73 @@
-        const key = "YOUR_PUBLISHABLE_KEY";
-        const magic = new Magic(key);
+const key = "pk_test_E90FA292AE61C345";
+const magic = new Magic(key);
 
-        const render = async() => {
-            const isLoggedIn = await magic.user.isLoggedIn();
-            /* Show login form if user is not logged in */
-            let html = `
+const render = async() => {
+    const isLoggedIn = await magic.user.isLoggedIn();
+    /* Show login form if user is not logged in */
+    let html = `
                 <h1>Please sign up or login</h1>
                 <form onsubmit="handleLogin(event)">
                 <input type="email" name="email" required="required" placeholder="Enter your email" />
                 <button type="submit">Send</button>
                 </form>
             `;
-            if (isLoggedIn) {
-                /* Get user metadata including email */
-                const userMetadata = await magic.user.getMetadata();
+    if (isLoggedIn) {
+        /* Get user metadata including email */
+        const userMetadata = await magic.user.getMetadata();
 
-                console.log(userMetadata);
-                const token = await magic.user.getIdToken();
+        console.log(userMetadata);
+        const token = await magic.user.getIdToken();
 
-                html = `
+        html = `
                 <h1>Current user: ${userMetadata.email}</h1>
                 <button onclick="handleLogout()">Logout</button>
                 <button onclick="verify('${token}')">Verify</button>
                 `;
-            }
-            document.getElementById("app").innerHTML = html;
-        };
+    }
+    document.getElementById("app").innerHTML = html;
+};
 
-        const handleLogin = async(e) => {
-            e.preventDefault();
-            const email = new FormData(e.target).get("email");
-            if (email) {
-                /* One-liner login 🤯 */
-                await magic.auth.loginWithMagicLink({
-                    email,
-                });
-                render();
-            }
-        };
-
-        const handleLogout = async() => {
-            await magic.user.logout();
-            render();
-        };
-
-        function verify(token) {
-            var bearer = "Bearer " + token;
-            opts = {
-                name: "test",
-            };
-            fetch("api/verify", {
-                    body: JSON.stringify(opts),
-                    method: "POST",
-                    withCredentials: true,
-                    credentials: "include",
-                    headers: {
-                        Authorization: bearer,
-                        "Content-Type": "application/json",
-                    },
-                })
-                .then((resp) => {
-                    return resp.text();
-                })
-                .then((data) => {
-                    console.log(data);
-                })
-                .catch((error) => {
-                    console.log("error: ", error);
-                });
-        }
-
+const handleLogin = async(e) => {
+    e.preventDefault();
+    const email = new FormData(e.target).get("email");
+    if (email) {
+        /* One-liner login 🤯 */
+        await magic.auth.loginWithMagicLink({
+            email,
+        });
         render();
+    }
+};
+
+const handleLogout = async() => {
+    await magic.user.logout();
+    render();
+};
+
+function verify(token) {
+    var bearer = "Bearer " + token;
+    opts = {
+        name: "test",
+    };
+    fetch("api/verify", {
+            body: JSON.stringify(opts),
+            method: "POST",
+            withCredentials: true,
+            credentials: "include",
+            headers: {
+                Authorization: bearer,
+                "Content-Type": "application/json",
+            },
+        })
+        .then((resp) => {
+            return resp.text();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.log("error: ", error);
+        });
+}
+
+render();
